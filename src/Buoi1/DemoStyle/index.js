eb02538ea9e43fb2e1a722d26b769d085ff020f9
Dat Huynh <huynhtuandat2997@gmail.com>
2021-10-14 00:05:03 +0700

    Lear StyleSheet && Image & TextInput

--- src/Buoi1/DemoStyle/index.js
+++ src/Buoi1/DemoStyle/index.js
@@ -1,24 +1,40 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 
 export default class DemoStyle extends Component {
   render() {
     return (
-      <View style={{marginTop: 50, paddingLeft: 10, paddingRight: 10}}>
-        <View
-          style={{
-            backgroundColor: '#C9C9FF',
-            height: 200,
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderRadius: 10,
-          }}>
-          <Text>Purple: #C9C9FF</Text>
+      <View style={{marginTop: 50}}>
+        <View style={boxStyle}>
+          <Text style={textStyle}>Purple:#C9C9Ff</Text>
         </View>
-        <View>
-          <Text>Blue: #3D85C6</Text>
+        <View style={boxStyle}>
+          <Text style={textStyle}>Blue:#3D85C6 </Text>
+        </View>
+
+        <View style={boxStyle}>
+          <Text style={textStyle}>Green: #96CC96</Text>
+        </View>
+        <View style={boxStyle}>
+          <Text style={textStyle}>Yellow: #F4B940</Text>
         </View>
       </View>
     );
   }
 }
+
+const boxStyle = {
+  borderRadius: 10,
+  backgroundColor: '#C9C9Ff',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: 150,
+  marginTop: 10,
+  marginLeft: 20,
+  marginRight: 20,
+};
+
+const textStyle = {
+  fontWeight: '900',
+};
