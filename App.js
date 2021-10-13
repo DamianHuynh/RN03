f489e4175e5bb032aaaa9c55a4da81dbff343606
Dat Huynh <huynhtuandat2997@gmail.com>
2021-10-14 00:03:25 +0700

    Lear StyleSheet && Image & TextInput

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import React, {Component} from 'react';
 import DemoComponentDefault from './src/Buoi1/DemoComponent';
 import {DemoComponentNonDefault} from './src/Buoi1/DemoComponentNonDefault';
 import DemoStyle from './src/Buoi1/DemoStyle';
+import DemoStyleSheet from './src/Buoi2/DemoStyleSheet';
+import DemoFlex from './src/Buoi2/DemoFlex';
+import ExLayout from './src/Buoi2/ExLayout';
+import DemoImage from './src/Buoi2/DemoImage';
 
 //Stateful Component
 class App extends Component {
   render() {
-    return <DemoStyle />;
+    return <DemoImage />;
   }
 }
 
